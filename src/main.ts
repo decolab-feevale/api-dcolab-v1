2e0051237c9eadd65c9d6ba6b9ccb89c203f289f
decolab-feevale <redefeevale@gmail.com>
2021-11-24 01:20:21 -0300

    new cfg cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
+  app.enableCors({credentials: true, origin: "http://localhost:19006"});
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
