7a5d0e3c8e79dfcd9e5d3a34b5d98a4426f0d84a
decolab-feevale <redefeevale@gmail.com>
2021-11-24 01:36:00 -0300

    http

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({credentials: true, origin: "http://localhost:19006"});
+  app.enableCors({credentials: true, origin: "http://192.168.1.103:19000"});
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
